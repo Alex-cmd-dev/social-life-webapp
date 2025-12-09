fda2f7d5b2a2aa8bb0326f4f24c28ace7dd25823
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-12-09 09:42:59 -0600

    config: Allow all hostnames for remote images (dev only)

--- next.config.ts
+++ next.config.ts
@@ -5,23 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.vercel-storage.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.cloudinary.com",
+        hostname: "**",
       },
     ],
   },
